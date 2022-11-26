df125b6eaa973465720c98ac2bffc7349e7b31ed
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2022-11-26 16:31:11 +0800

    Update Project Name, add double quotes to prettier, add .history files to gitignore

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,9 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  prettier: {
+    printWidth: 120,
+    singleQuote: false,
+    tabWidth: 2,
+  },
 };
