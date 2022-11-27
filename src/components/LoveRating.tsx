35ac2a402baad654904f33f6bb0fd15542b8d1a9
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2022-11-27 14:29:18 +0800

    Fixed react/HTML warnings

--- src/components/LoveRating.tsx
+++ src/components/LoveRating.tsx
@@ -5,66 +5,77 @@ export const LoveRating = ({ rating, id }: { rating: number; id: string }) => (
       name={`${id}-love-rating`}
       className="rating-hidden"
       checked={rating === 0}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-1 mask-heart bg-pink-500"
       checked={rating === 0.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-2 mask-heart bg-pink-500"
       checked={rating === 1}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-1 mask-heart bg-pink-500"
       checked={rating === 1.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-2 mask-heart bg-pink-500"
       checked={rating === 2}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-1 mask-heart bg-pink-500"
       checked={rating === 2.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-2 mask-heart bg-pink-500"
       checked={rating === 3}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-1 mask-heart bg-pink-500"
       checked={rating === 3.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-2 mask-heart bg-pink-500"
       checked={rating === 4}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-1 mask-heart bg-pink-500"
       checked={rating === 4.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-love-rating`}
       className="mask mask-half-2 mask-heart bg-pink-500"
       checked={rating === 5}
+      readOnly={true}
     />
   </div>
 );
--- src/components/SkillRating.tsx
+++ src/components/SkillRating.tsx
@@ -4,67 +4,77 @@ export const SkillRating = ({ rating, id }: { rating: number; id: string }) => (
       type="radio"
       name={`${id}-experience-rating`}
       className="rating-hidden"
-      checked={rating === 0}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-1 mask-star-2 bg-blue-500"
       checked={rating === 0.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-2 mask-star-2 bg-blue-500"
       checked={rating === 1}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-1 mask-star-2 bg-blue-500"
       checked={rating === 1.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-2 mask-star-2 bg-blue-500"
       checked={rating === 2}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-1 mask-star-2 bg-blue-500"
       checked={rating === 2.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-2 mask-star-2 bg-blue-500"
       checked={rating === 3}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-1 mask-star-2 bg-blue-500"
       checked={rating === 3.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-2 mask-star-2 bg-blue-500"
       checked={rating === 4}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-1 mask-star-2 bg-blue-500"
       checked={rating === 4.5}
+      readOnly={true}
     />
     <input
       type="radio"
       name={`${id}-experience-rating`}
       className="mask mask-half-2 mask-star-2 bg-blue-500"
       checked={rating === 5}
+      readOnly={true}
     />
   </div>
 );
