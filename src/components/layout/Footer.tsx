907a33b780aa0c257305c085dd20ae7303977534
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2022-11-27 22:19:11 +0800

    Updated Footer/Promotion

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import React from "react";
+import taipeiDevLogo from "../assets/taipei-dev/taipeidev-256.png";
 
 function Footer() {
   return (
@@ -6,82 +8,59 @@ function Footer() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="text-center xl:flex xl:items-center xl:justify-between xl:text-left">
           <div className="xl:flex xl:items-center xl:justify-start">
-            <img
-              className="mx-auto h-7 w-auto"
-              src="https://cdn.rareblocks.xyz/collection/celebration/images/logo-alt-2.svg"
-              alt=""
+            <Image
+              className="mx-auto h-12 w-auto rounded-full bg-white p-2"
+              src={taipeiDevLogo}
+              alt="Taipei.dev Logo"
             />
 
             <p className="mt-5 text-sm text-white xl:ml-6 xl:mt-0">
-              © Copyright 2021 Postcraft
+              Powered by{" "}
+              <a className="link font-bold" href="https://taipei.dev">
+                Taipei.dev
+              </a>{" "}
+              <br />
+              and{" "}
+              <a className="link" href="https://create.t3.gg">
+                Create T3 App
+              </a>
             </p>
           </div>
 
           <div className="mt-8 items-center xl:mt-0 xl:flex xl:justify-end xl:space-x-8">
-            <ul className="flex flex-wrap items-center justify-center gap-x-8 gap-y-3 xl:justify-end">
-              <li>
-                <a
-                  href="#"
-                  title=""
-                  className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
-                >
-                  {" "}
-                  About{" "}
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  title=""
-                  className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
-                >
-                  {" "}
-                  Services{" "}
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  title=""
-                  className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
-                >
-                  {" "}
-                  Privacy Policy{" "}
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  title=""
-                  className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
-                >
-                  {" "}
-                  Terms & Conditions{" "}
-                </a>
-              </li>
-
-              <li>
-                <a
-                  href="#"
-                  title=""
-                  className="text-sm text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
-                >
-                  {" "}
-                  Support{" "}
-                </a>
-              </li>
-            </ul>
-
-            <div className="mt-8 mb-5 h-px w-full bg-gray-50/20 xl:m-0 xl:h-6 xl:w-px"></div>
+            <div className="px-2 text-center text-sm text-white md:px-12">
+              If you would like to create a Portfolio site for yourself using
+              the <span className="font-bold">Portfolio-T3 App</span>, please
+              click here:
+              <br />
+              <a
+                className="link font-bold"
+                href="https://github.com/TaipeiDev/portfolio-t3"
+              >
+                Fork me on GitHub
+              </a>
+            </div>
 
             <ul className="flex items-center justify-center space-x-8 xl:justify-end">
               <li>
                 <a
-                  href="#"
-                  title=""
+                  href="https://github.com/TaipeiDev"
+                  title="Taipei.Dev Github Page"
+                  className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
+                >
+                  <svg
+                    viewBox="0 0 496 512"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 fill-current"
+                  >
+                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                  </svg>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/taipeidev"
+                  title="Taipei.dev Twitter Page"
                   className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
                 >
                   <svg
@@ -97,8 +76,8 @@ function Footer() {
 
               <li>
                 <a
-                  href="#"
-                  title=""
+                  href="https://www.facebook.com/profile.php?id=100088267535458"
+                  title="Taipei.dev Facebook Page"
                   className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
                 >
                   <svg
@@ -111,22 +90,18 @@ function Footer() {
                   </svg>
                 </a>
               </li>
-
               <li>
                 <a
-                  href="#"
-                  title=""
+                  href="mailto:hi@taipei.dev"
+                  title="Taipei.dev Email"
                   className="block text-white transition-all duration-200 hover:text-opacity-80 focus:text-opacity-80"
                 >
                   <svg
-                    className="h-6 w-6"
+                    viewBox="0 0 512 512"
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
+                    className="h-4 w-4 fill-current"
                   >
-                    <path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path>
-                    <circle cx="16.806" cy="7.207" r="1.078"></circle>
-                    <path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path>
+                    <path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm0 48v40.805c-22.422 18.259-58.168 46.651-134.587 106.49-16.841 13.247-50.201 45.072-73.413 44.701-23.208.375-56.579-31.459-73.413-44.701C106.18 199.465 70.425 171.067 48 152.805V112h416zM48 400V214.398c22.914 18.251 55.409 43.862 104.938 82.646 21.857 17.205 60.134 55.186 103.062 54.955 42.717.231 80.509-37.199 103.053-54.947 49.528-38.783 82.032-64.401 104.947-82.653V400H48z"></path>
                   </svg>
                 </a>
               </li>
